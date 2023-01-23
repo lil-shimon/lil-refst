724fc03d9c204ac99418cba0246e0b7503d4fb9d
lil-shimon <shimon101018@gmail.com>
2023-01-24 00:56:43 +0900

    feat: 🎸 resp, req

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,7 +2,24 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
-    id: int
+class TaskBase(BaseModel):
     title: Optional[str] = Field(None, example="歯磨きをする")
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="完了したか")
+
+    class Config:
+        orm_mode = True
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
